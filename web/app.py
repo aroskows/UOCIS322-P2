7241b38f265eb15728af5b955b3a546b3ab30614
Alexa Roskowski <aroskows@testium.cs.uoregon.edu>
2021-04-25 15:22:51 -0700

    finished project 2

--- web/app.py
+++ web/app.py
@@ -1,14 +1,38 @@
 """
-John Doe's Flask API.
+Alexa Roskowski's Flask API.
 """
 
-from flask import Flask
+from flask import Flask, abort, render_template
+import os
 
 app = Flask(__name__)
 
+@app.errorhandler(404)
+def not_found(e):
+    return render_template('404.html')
+
+@app.errorhandler(403)
+def forbidden(e):
+    return render_template('403.html')
+
+@app.route("/<path:name>")
+def logic(name):
+    if "~" in name or "//" in name or ".." in name:
+        #break --> give a 403 error code
+        abort(403)
+    elif name[-5:] == '.html' or name[-4:] == '.css':
+        if name in os.listdir(os.curdir):
+            f = open(name, "r") 
+            return f.read()
+        else:
+            abort(404)
+    else: 
+        return "UOCIS docker demo! \n"
+
 @app.route("/")
 def hello():
-    return "UOCIS docker demo!\n"
+     return "UOCIS docker demo! \n"
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
